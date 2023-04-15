51b0a6b5999143fad7383c172c43e5385bf29ac7
Garrett Merz <gmerz2@olvi-2.datascience.wisc.edu>
2023-04-14 21:18:35 -0500

    add runpeft

--- runpeft.py
+++ runpeft.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# coding: utf-8
 
+import pandas as pd
 from gptchem.gpt_classifier import GPTClassifier
 from gptchem.tuner import Tuner
 from gptchem.formatter import RegressionFormatter
-
-import pandas as pd 
-import os
-from peft import PeftModel
-from transformers import GenerationConfig
 from sklearn.model_selection import train_test_split
-#import matplotlib.pyplot as plt
+
+
+import matplotlib.pyplot as plt
+
 from scipy.stats import pearsonr
 import pickle
 import json
-import sys
-from typing import List
-import torch
-import transformers
-
-from peft import (
-    LoraConfig,
-    get_peft_model,
-    get_peft_model_state_dict,
-    prepare_model_for_int8_training,
-    set_peft_model_state_dict,
-)
-
-from datasets import Dataset
-from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, EarlyStoppingCallback
-
 TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING = {
     "t5": ["q", "v"],
     "mt5": ["q", "v"],
@@ -50,7 +34,27 @@ TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING = {
     "llama": ["q_proj", "v_proj"],
     "chatglm": ["query_key_value"],
 }
+import sys
+from typing import List
+import torch
+import transformers
 
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    prepare_model_for_int8_training,
+    set_peft_model_state_dict,
+    PeftModel,
+)
+from datasets import Dataset
+import torch
+#from utils.callbacks import Iteratorize, Stream
+
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, EarlyStoppingCallback,GenerationConfig
+import os
+print(torch.cuda.device_count())
+#get the data
 def get_data(prop_to_get):
     
     prop = {"b3lyp": "B3LYP atomization energy in kcal/mol",
@@ -82,10 +86,12 @@ def get_data(prop_to_get):
                    columns =["SMILES", prop["b3lyp"], prop["g4mp2"], prop["en_diff"], prop["bandgap"]])
         return df
     
+    #unpickle the data
     train_df = pickle_to_df("qm9_key_smiles_1_train_data_without_validation.pickle")
     val_df = pickle_to_df("qm9_key_smiles_1_validation_data.pickle")
     test_df = pickle_to_df("qm9_key_smiles_1_holdout_data.pickle")
     
+    #format the data as text for the LLM
     formatter = RegressionFormatter(representation_column='SMILES',
         label_column=prop[prop_to_get],
         property_name=prop[prop_to_get],
@@ -97,8 +103,6 @@ def get_data(prop_to_get):
     df_test = formatter.format_many(test_df).drop(columns=["label","representation"], axis=1)
     
     return df_train, df_val, df_test
-
-
 def train(
     #dataframes
     df_train: pd.DataFrame,
@@ -108,8 +112,7 @@ def train(
     prop: str = "b3lyp",
     output_dir: str = "outputs",
     # training hyperparams
-    batch_size: int = 1024,
-    micro_batch_size: int = 64,
+    batch_size: int = 256,
     num_epochs: int = 20,
     learning_rate: float = 3e-4,
     cutoff_len: int = 256,
@@ -133,7 +136,6 @@ def train(
             f"prop: {prop}\n"
             f"output_dir: {output_dir}_{prop}_{base_model}\n"
             f"batch_size: {batch_size}\n"
-            f"micro_batch_size: {micro_batch_size}\n"
             f"num_epochs: {num_epochs}\n"
             f"learning_rate: {learning_rate}\n"
             f"cutoff_len: {cutoff_len}\n"
@@ -149,27 +151,30 @@ def train(
     assert (
         base_model
     ), "Please specify a --base_model, e.g. --base_model='gpt2'"
-    gradient_accumulation_steps = batch_size // micro_batch_size
+    #
+    #gradient_accumulation_steps = 1
 
-    device_map = "sequential"
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    #tells us how to split the model between GPUs 
+    #device_map = "sequential"
+    #world_size = int(os.environ.get("WORLD_SIZE", 1))
     #ddp = world_size != 1
-    ddp = False
-    if ddp:
-        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
-        gradient_accumulation_steps = gradient_accumulation_steps // world_size
+    #ddp = False
+    #if ddp:
+    #    device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+    #    gradient_accumulation_steps = gradient_accumulation_steps // world_size
     
     #set up the model and tokenizer    
     tokenizer = AutoTokenizer.from_pretrained(base_model)
-    # might not be optimal, just trying to run the code
     tokenizer.pad_token = tokenizer.eos_token
 
+    #get the model with the desired name automatically
     model = AutoModelForCausalLM.from_pretrained(
         base_model, 
         load_in_8bit=False,
         torch_dtype=torch.float16,
         device_map='sequential',
     )    
+    #tokenizer settings
     def tokenize(prompt):
         result = tokenizer(
             prompt,
@@ -179,7 +184,7 @@ def train(
             return_tensors=None,
         )
         return result
-    
+    #tokenize the full prompt
     def tokenize_prompt(data_point):
         full_prompt = data_point["prompt"]+data_point["completion"]
         tokenized_full_prompt = tokenize(full_prompt)
@@ -199,7 +204,6 @@ def train(
 
     train_data = Dataset.from_pandas(df_train).shuffle().map(tokenize_prompt)
     val_data = Dataset.from_pandas(df_val).shuffle().map(tokenize_prompt)
-    test_data = Dataset.from_pandas(df_test).shuffle().map(tokenize_prompt)
     
     if resume_from_checkpoint:
         # Check the available weights and load them
@@ -222,34 +226,32 @@ def train(
             print(f"Checkpoint {checkpoint_name} not found")
 
     model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
-    
+    print(train_data[0])
+    print(val_data[0])
+    """
     if not ddp and torch.cuda.device_count() > 1:
         # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
         model.is_parallelizable = True
         model.model_parallel = True
-   
+    """
     trainer = transformers.Trainer(
         model=model,
         train_dataset=train_data,
         eval_dataset=val_data,
         args=transformers.TrainingArguments(
-            per_device_train_batch_size=micro_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
+            per_device_train_batch_size=batch_size,
             warmup_steps=10,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
             fp16=True,
-            logging_steps=4,
             optim="adamw_torch",
             evaluation_strategy="steps",
             save_strategy="steps",
-            eval_steps=4,
-            save_steps=4,
             output_dir=output_dir+"_"+prop+"_"+base_model,
             save_total_limit=3,
             metric_for_best_model = 'eval_loss',
             load_best_model_at_end=True,
-            ddp_find_unused_parameters=False if ddp else None,
+            #ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
         ),
         data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
@@ -277,7 +279,6 @@ def train(
         "\n If there's a warning about missing keys above, please disregard :)"
     )
 
-
 def generate(
     df_test: pd.DataFrame,
     load_8bit: bool = False,
@@ -287,11 +288,12 @@ def generate(
     prompt_template: str = "",
     cutoff_len: int = 256,
 ):
+
     if torch.cuda.is_available():
         device = "cuda"
     else:
         device = "cpu" 
-    
+
     #set up the model and tokenizer    
     tokenizer = AutoTokenizer.from_pretrained(base_model)
     # might not be optimal, just trying to run the code
@@ -311,7 +313,7 @@ def generate(
         base_model, 
         load_in_8bit=False,
         torch_dtype=torch.float16,
-        device_map='sequential',
+        device_map='auto',
     )    
     model = PeftModel.from_pretrained(
         model,
@@ -331,7 +333,7 @@ def generate(
         temperature=0,
         top_p=0.75,
         top_k=40,
-        num_beams=4,
+        num_beams=2,
         max_new_tokens=128,
         stream_output=False,
         **kwargs,
@@ -355,7 +357,7 @@ def generate(
             "max_new_tokens": max_new_tokens,
         }
 
-        # Without streaming
+        # Generate outs without streaming
         with torch.no_grad():
             generation_output = model.generate(
                 input_ids=input_ids,
@@ -368,27 +370,73 @@ def generate(
         output = tokenizer.decode(s)
         #print(output)
         return output
-    
+
     #convert to a number if we can, else none
     def toFloat(x):
         try:
             return float(x)
         except:
             return None 
-
+    
     df_test["model_out"] = df_test["prompt"].map(lambda x: evaluate(x))
     df_test["energy_out"] = df_test["model_out"].map(lambda x: toFloat(x.replace('###','@@@').split('@@@')[1]))
     df_test["energy_true"] = df_test["completion"].map(lambda x: toFloat(x.split('@@@')[0]))
-    
     return(df_test)
 
-if __name__ == "__main__":
-    df_train, df_val, df_test = get_data("b3lyp")
-    print(df_train.head(10))
-    print(df_val.head(10))
-    print(df_test.head(10))
-    train(df_train, df_val, base_model="gpt2", prop="b3lyp")
-    outs=generate(df_test.head(1000), base_model="gpt2", prop="b3lyp")
-    outs.to_json(f"outputs_gpt2_b3lyp_1000_e20.json")
+def run_all(model):
+    df_train,df_val,df_test = get_data("b3lyp")
+    train(df_train, df_val, base_model=model, prop="b3lyp")
+    outs=generate(df_test.head(1000), base_model=model, prop="b3lyp")
+    outs.to_json(f"outputs_{model}_b3lyp.json")
+    outs=outs.dropna()
+    plt.scatter(outs['energy_true'], outs['energy_out'])
+    pearsonr(outs['energy_true'], outs['energy_out'])
+    plt.axis('square')
+    plt.savefig("outputs_{model}_b3lyp.png")
+    plt.clf()
+
+    df_train,df_val,df_test = get_data("g4mp2")
+    train(df_train, df_val, base_model=model, prop="g4mp2")
+    outs=generate(df_test.head(1000), base_model=model, prop="g4mp2")
+    outs.to_json(f"outputs_{model}_g4mp2.json")
+    outs=outs.dropna()
+    plt.scatter(outs['energy_true'], outs['energy_out'])
+    pearsonr(outs['energy_true'], outs['energy_out'])
+    plt.axis('square')
+    plt.savefig("outputs_{model}_g4mp2.png")
+    plt.clf()
+
+    df_train,df_val,df_test = get_data("en_diff")
+    train(df_train, df_val, base_model=model, prop="en_diff")
+    outs=generate(df_test.head(1000), base_model=model, prop="en_diff")
+    outs=outs.dropna()
+    outs.to_json(f"outputs_{model}_en_diff.json")
+    plt.scatter(outs['energy_true'], outs['energy_out'])
+    pearsonr(outs['energy_true'], outs['energy_out'])
+    plt.axis('square')
+    plt.savefig("outputs_{model}_endiff.png")
+    plt.clf()
+
+    df_train,df_val,df_test = get_data("bandgap")
+    train(df_train, df_val, base_model=model, prop="bandgap")
+    outs=generate(df_test.head(1000), base_model=model, prop="bandgap")
+    outs.to_json(f"outputs_{model}_bandgap.json")
+    outs=outs.dropna()
+    plt.scatter(outs['energy_true'], outs['energy_out'])
+    pearsonr(outs['energy_true'], outs['energy_out'])
+    plt.axis('square')
+    plt.savefig("outputs_{model}_bandgap.png")
+    plt.clf()
+
+if __name__ == '__main__':
+     run_all("gpt2")
+
+
+
+
+
+
+
+
 
 
